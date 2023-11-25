339cc848715939154083d00c5215f2ec04b969ae
Luis Ruiz <luis.a.r.0212@gmail.com>
2023-11-24 16:47:19 -0800

    more fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter as Router, Route, Routes, HashRouter } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import "./styles.css";
 
 import App from "./App";
@@ -8,10 +8,12 @@ import Character from "./Characters";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
-  <HashRouter basename="/">
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/api" element={<Character />} />
-    </Routes>
-  </HashRouter>
+  <Router>
+    <div>
+      <Routes>
+        <Route exact path="/" element={<App />} />
+        <Route path="/api" element={<Character />} />
+      </Routes>
+    </div>
+  </Router>
 );
