459a2badd100202d17c664fc08a3eb67d1d50e47
Luis Ruiz <luis.a.r.0212@gmail.com>
2023-11-24 17:31:41 -0800

    please work

--- src/index.js
+++ src/index.js
@@ -9,12 +9,21 @@ import Character from "./Characters";
 const root = createRoot(document.getElementById("root"));
 root.render(
   <Router>
-    
-    <div>
-      <Routes>
-        <Route exact path="/" element={<App />} />
-        <Route path="/api" element={<Character />} />
-      </Routes>
+    <div className="container">
+      <header>
+        <h1>Homework 6</h1>
+        <nav>
+          <ul>
+            <li><a href="/">Home</a></li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <Routes>
+          <Route exact path="/" element={<App />} />
+          <Route path="/api" element={<Character />} />
+        </Routes>
+      </main>
     </div>
   </Router>
 );
