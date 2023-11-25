468fe3a5d27ca4cd18c1797cc62ffd5f493bb689
Luis Ruiz <luis.a.r.0212@gmail.com>
2023-11-24 16:32:29 -0800

    new stuff

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, HashRouter } from 'react-router-dom';
 import "./styles.css";
 
 import App from "./App";
@@ -8,10 +8,10 @@ import Character from "./Characters";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
-  <Router>
+  <HashRouter basename="/">
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/api" element={<Character />} />
     </Routes>
-  </Router>
+  </HashRouter>
 );
